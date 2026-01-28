d0ebf29a214e79862713b854e1ced70b38f1265d
Mars <tngus9536@gmail.com>
2026-01-28 15:06:13 +0900

    chore: initial android hybrid project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.devtoolsKsp)
+}
+
+android {
+    namespace = "com.mars.hybrid.aos"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.mars.hybrid.aos"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/mars/hybrid/aos/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.mars.hybrid.aos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * ======================================================
+ * Title      : MainActivity
+ * Developer  : Mars
+ * Date       : 2026-01-28
+ * Description:
+ *  - Entry point of the Android Hybrid application.
+ *  - This activity hosts the WebView and connects the Web layer with native Android features via a JavaScript bridge.
+ *
+ * History:
+ *  - [2026.01.28] MainActivity Initialization
+ * ======================================================
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
